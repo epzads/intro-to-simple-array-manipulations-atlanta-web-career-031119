de253add93512264db2d9d6bf76b5b4c06fd1d87
favorable-vector-1743 <epzads@gmail.com>
2019-03-03 17:59:12 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,53 @@
+def using_push(array,string)
+  
+array.push(string)
+
+end   
+
+def using_unshift(array,string)
+  
+  array.unshift(string)
+  
+end 
+
+def using_pop(array,string)
+  
+  return array.pop
+end 
+
+def pop_with_args(array)
+  array.pop
+  array.pop 
+end 
+
+def using_shift(array)
+  return array.shift 
+  
+end   
+
+
+def shift_with_args(array)
+  return array.shift(2)
+  
+end 
+
+def using_concat(array,array2)
+  
+  return array.concat(array2)
+end 
+
+def using_insert(array,element)
+ array.insert(4,element)
+  
+end   
+
+def using_uniq(array)
+  
+  array.uniq 
+ end  
+  
+
+def using_flatten(array)
+ return  arrray.flatten 
+end   
+  
